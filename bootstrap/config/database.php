415f5e7f53c648a0bf93bc299d7cfc1ef7c35ed0
chen <cindy31224@gmail.com>
2020-05-27 15:01:02 +0800

    暫存

--- bootstrap/config/database.php
+++ bootstrap/config/database.php
@@ -4,7 +4,7 @@ return [
     'host' => '127.0.0.1',
     'port' => '8889',
     'charset' => 'utf8',
-    'database' => 'sa',
+    'database' => 'heybus',
     'username' => 'root',
     'password' => 'root',
 ];
--- bootstrap/services/sql.php
+++ bootstrap/services/sql.php
@@ -1,37 +1,108 @@
 <?php
     require  'db.php';
     session_start();
-    
-    function fetchAllUser($conn)
-{
-    $stmt = $conn->prepare('SELECT * FROM bus');
-    $stmt->execute();
-    
-    return $stmt->fetchAll(PDO::FETCH_CLASS,'BUS');
-}
-    
+    //找公車
+    function findroutebystop($conn, $search)
+    {
+        $stmt =$conn->prepare("SELECT route_name from `route_stop as rs,route as r,stop as s` WHERE `stop_name` LIKE :search 
+        And s.stop_id=rs.stop_id And rs.route_id=r.route_id");
+        $stmt->execute();
 
-    function findDriverByBUS($conn, $d_account)
-{
-    $stmt = $conn->prepare('SELECT * FROM bus where d_account = :d_account');
-    $stmt->bindParam(':d_account', $d_account);
-    $stmt->execute();
+        $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-    return $stmt->fetch(PDO::FETCH_ASSOC);
-}
+        return $stmt;
 
-function findStopByBUS($conn, $d_account)
-{
-    $stmt = $conn->prepare("SELECT * from `stop` where stop_id in 
-                                (select stop_id from `route_stop` where route_id = 
-                                    (select route_id from `bus` where d_account = '$d_account'))");
-    $stmt->execute();
+    }
+    function findroute($conn, $search)
+    {
+        $stmt =$conn->prepare("SELECT route_name from `route` Where`route_name` Like :search");
+        $stmt->execute();
 
-    $stoplist = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-    return $stoplist;
-}
+        return $stmt;
 
+    }
+    //註冊
+    function addpassenger($conn, $passenger_account,$passenger_password,$passenger_name)
+    {
+        $stmt =$conn->prepare("INSERT INTO users (passenger_account, passenger_password, passenger_name) VALUES (:passenger_account, :passenger_password, :passenger_name) ");
+        $stmt->execute([
+            'account' => $passenger_account,
+            'password' => $passenger_password,
+            'name' => $passenger_name
+        ]);
+
+        $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        return $stmt;
+
+    }
+    //登入
+    function findpassengerid($conn, $passenger_account)
+    {
+        $stmt =$conn->prepare("SELECT * from `passenger` Where passenger_account =:passenger_account ");
+        $stmt->bindParam(':passenger_account', $passenger_account);
+        $stmt->execute();
+        $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        return $stmt;
+
+    }
+    function finddriverid($conn, $driver_account)
+    {
+        $stmt =$conn->prepare("SELECT * from `driverr` Where driver_account =:driverr_account ");
+        $stmt->bindParam(':driver_account', $driver_account);
+        $stmt->execute();
+        $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        return $stmt;
+
+    }
+    //黑名單
+    function addblacktime($conn, $data)
+    {
+        $sql = "INSERT INTO `black_list` (`passenger_id`, `black_time`) VALUES (:passenger_id, :black_time) ";
+    $stmt = $conn->prepare($sql);
+    $blackData = [
+        'passenger_id'       => $data['passenger_id'], /**完成資料綁定 */
+        'black_time'    => $data['black_time'], /**完成資料綁定 */
+    ];
+    return $stmt->execute($blackData);
+
+    }
+    function findblacktime($conn, $passenger_id)
+    {
+        $stmt =$conn->prepare("SELECT * from `black_list` Where `passenger_id`=$passenger_id");
+        $stmt->execute();
+
+        $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        return $stmt;
+
+    }
+    //上下車
+    function findgeton($conn, $passenger_id)
+    {
+        $stmt =$conn->prepare("SELECT * from `geton` Where `passenger_id`=$passenger_id");
+        $stmt->execute();
+
+        $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        return $stmt;
+
+    }
+    function findgetoff($conn, $passenger_id)
+    {
+        $stmt =$conn->prepare("SELECT * from `getoff` Where `passenger_id`=$passenger_id");
+        $stmt->execute();
+
+        $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        return $stmt;
+
+    }
+    //以下不是
 function stoplistname($conn, $d_account)
 {
     $stmt = $conn->prepare("SELECT stop_name from `stop` where stop_id in 
@@ -59,7 +130,7 @@ function updatedepart($conn, $d_account, $data=[])
 }
 
 function updatenextstop($conn, $d_account, $data=[])
-{    
+{ 
     $sql = "UPDATE bus set `nextstop`=:nextstop where d_account = '{$d_account}'";
     //var_dump($d_account);
     $stmt = $conn->prepare($sql);
