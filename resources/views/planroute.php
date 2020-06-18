28e61716d5609f77576461e46b4da9e5a2204389
Alexandra Lee <liyun@leeyundeMacBook-Air.local>
2020-06-18 17:09:19 +0800

    planroute新增

--- resources/views/planroute.php
+++ resources/views/planroute.php
@@ -79,18 +79,16 @@
 <table align="center" style="text_align:center">
 <tr>
 <td>
-  <input type="text" placeholder="請輸入起點站牌名稱" style="margin: 10px;"><br>
+  <input type="text" placeholder="請輸入起點站牌名稱" style="margin: 10px;" value="start"><br>
   <span class="material-icons" style="color: black; position:relative;left:100px;top:5px">arrow_downward</span><br>
-  <input type="text" placeholder="請輸入終點站牌名稱" style="margin: 10px">
+  <input type="text" placeholder="請輸入終點站牌名稱" style="margin: 10px" value="goal">
+  <p></p>
+  <button class="" type="submit" name="規劃路線" value="plan" class="form-control btn btn-primary" style="position: relative; left:27%" onclick="/planroute" method="post">規劃路線</button>
 </td>
 <td><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d255423.3793886854!2d121.38428615039757!3d25.03870377727416!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442ac72bce20a99%3A0x3f6a35cedd0ac2e0!2z5Y-w5YyX!5e0!3m2!1szh-TW!2stw!4v1590044977742!5m2!1szh-TW!2stw" width="800" height="520" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe></td>
     
     
 </tr>
-<td></td>
-  <tr>
-    
-  </tr>
 </table>
 
 <!--
